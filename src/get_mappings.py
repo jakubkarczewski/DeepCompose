7d615a6b6ed337afbc3b5c0b89889c91f079bfa3
jakubkarczewski <jakub.jan.karczewski@intel.com>
2019-01-09 19:47:03 +0100

    Bad code 4 real

--- src/get_mappings.py
+++ src/get_mappings.py
@@ -1,3 +1,4 @@
+import argparse
 from os import listdir
 from os.path import join
 from pickle import dump
@@ -5,32 +6,48 @@ from pickle import dump
 
 from midi import Polyphonic_pianoroll, parse_midi, save_midi
 
-all_chords = []
-midi_dir = 'all_files/all_songs/'
 
-for filename in listdir(midi_dir):
-    if filename.endswith('.mid'):
+def main(training_dir, inference_path):
+    all_chords = []
 
-        tracks = parse_midi(join(midi_dir, filename), 40)
+    for filename in listdir(training_dir):
+        if filename.endswith('.mid'):
 
-        song = Polyphonic_pianoroll(tracks)
-        pianoroll = list(map(tuple, song.polyphonic_pianoroll))  # tuple is hashable
+            tracks = parse_midi(join(training_dir, filename), 40)
 
-        chords = sorted(list(set(pianoroll)))
+            song = Polyphonic_pianoroll(tracks)
+            pianoroll = list(map(tuple, song.polyphonic_pianoroll))  # tuple is hashable
 
-        # for i, chord in enumerate(chords):
-        #     if chord not in int_to_chord.values():
-        #         index = i + len(int_to_chord.values())
-        #         chord_to_int[chord] = index
-        #         int_to_chord[index] = chord
+            chords = sorted(list(set(pianoroll)))
 
-        for chord in chords:
-            if chord not in all_chords:
-                all_chords.append(chord)
+            for chord in chords:
+                if chord not in all_chords:
+                    all_chords.append(chord)
 
-int_to_chord = {i: chord for i, chord in enumerate(all_chords)}
-chord_to_int = {chord: i for i, chord in enumerate(all_chords)}
+    tracks = parse_midi(inference_path, 40)
 
-for filename, obj in zip(("int_to_chord.pkl", "chord_to_int.pkl"), (int_to_chord, chord_to_int)):
-    with open(filename, 'wb') as f:
-        dump(obj, f)
+    song = Polyphonic_pianoroll(tracks)
+    pianoroll = list(map(tuple, song.polyphonic_pianoroll))  # tuple is hashable
+
+    chords = sorted(list(set(pianoroll)))
+
+    for chord in chords:
+        if chord not in all_chords:
+            all_chords.append(chord)
+
+    int_to_chord = {i: chord for i, chord in enumerate(all_chords)}
+    chord_to_int = {chord: i for i, chord in enumerate(all_chords)}
+
+    for filename, obj in zip(("int_to_chord.pkl", "chord_to_int.pkl"), (int_to_chord, chord_to_int)):
+        with open(filename, 'wb') as f:
+            dump(obj, f)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--training_dir", type=str, required=True,
+                        help="dir with training data.")
+    parser.add_argument("--inference_path", type=str, required=True,
+                        help="path to file 4 inference")
+    args = parser.parse_args
+
